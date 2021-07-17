e2710d8162dad19f34c8ed5d77cd074af1f8d85b
BADBOY671 <86197607+BADBOY671@users.noreply.github.com>
2021-07-17 22:26:37 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const port = "8080"
+app.listen(port, function(){
+    console.log(`Your App Is listening  on ${port} port`)
+})
+var path = require("path");
+var bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+app.use(express.static("public"));
+
+
+
+
+app.get('/', async function(req,res){
+const serv = client.guilds.cache.get('800986021675794513')
+
+
+res.render("index.ejs", {
+    client,
+    serv,
+    
+})
+})
+
+
+client.login('Your Discord Bot Token Here')
